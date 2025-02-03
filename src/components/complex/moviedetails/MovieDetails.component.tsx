4d1b215da1c5894316d9ac5cad06c6b6a686981c
Stevic-Nenad <stevic.nenad@icloud.com>
2025-02-03 13:50:15 +0100

    created movie detials component to show some information about a clicked movie

---
+++ src/components/complex/moviedetails/MovieDetails.component.tsx
@@ -0,0 +1,11 @@
+import "./MovieDetails.style.scss";
+
+const MovieDetails = () => {
+  return(
+    <div className="movie-details">
+      Movie Details
+    </div>
+  );
+}
+
+export default MovieDetails;
